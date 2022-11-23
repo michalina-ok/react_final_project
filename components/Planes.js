8bd9a5f3cc46088be11b1cb02e31787a9a323b20
Michalina <michalina.oniszczuk@gmail.com>
2022-11-23 16:15:14 +0100

    added Planes component

---
+++ components/Planes.js
@@ -0,0 +1,12 @@
+function Planes(props) {
+    const planes = Math.round(props.inputs / 2000);
+  return (
+    <div className="trees">
+        <h2>Total yearly footprint is {props.inputs}g of CO2</h2>
+        <p>This is an equivalent of flying {planes} miles</p>
+        <img src="/plane.svg" alt="Icon of a plane"></img>
+        </div>
+  )
+}
+
+export default Planes
