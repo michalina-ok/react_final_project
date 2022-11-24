5373da1f12a2b1bb0f65286c83a41727d3d6b2cb
caroline cloughley <caro672g@stud.kea.dk>
2022-11-24 12:25:55 +0100

    arrows work here

--- components/Trees.js
+++ components/Trees.js
@@ -1,14 +1,15 @@
-
+import Planes from "./Planes";
 
 function Trees(props) {
-    const trees = Math.round(props.inputs / 2000);
+  const trees = Math.round(props.inputs / 2000);
+
   return (
-    <div className="trees">
-        <h2>Total yearly footprint is {props.inputs}g of CO2</h2>
-        <p>To offset this amount of CO2 {trees} trees need to be planted</p>
-        <img src="/tree.svg" alt="Icon of a tree"></img>
-        </div>
-  )
+    <>
+      <h2>Total yearly footprint is {props.inputs}g of CO2</h2>
+      <p>To offset this amount of CO2 {trees} trees need to be planted</p>
+      <img src="/tree.svg" alt="Icon of a tree"></img>
+    </>
+  );
 }
 
-export default Trees
+export default Trees;
